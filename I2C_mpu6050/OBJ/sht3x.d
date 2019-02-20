2fb5b7eec546bb0928004132466c0ee22ef33268
darboy <974782852@qq.com>
2019-02-20 18:30:42 +0800

    remove

--- I2C_mpu6050/OBJ/sht3x.d
+++
@@ -1,8 +0,0 @@
-..\obj\sht3x.o: ..\sht3x\sht3x.c
-..\obj\sht3x.o: ..\sht3x\sensirion_arch_config.h
-..\obj\sht3x.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-..\obj\sht3x.o: ..\sht3x\sensirion_common.h
-..\obj\sht3x.o: ..\sht3x\sensirion_i2c.h
-..\obj\sht3x.o: ..\sht3x\sht.h
-..\obj\sht3x.o: ..\sht3x\git_version.h
-..\obj\sht3x.o: ..\sht3x\sht_common.h
